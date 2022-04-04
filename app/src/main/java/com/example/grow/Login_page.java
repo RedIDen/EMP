60b97ee77f9a91bc90a4f2df0367c8851c654827
denios.vasilios@gmail.com <denios.vasilios@gmail.com>
2022-04-04 14:15:58 +0300

    Add Liza's registration and authorization via Firebase.

--- app/src/main/java/com/example/grow/Login_page.java
+++ app/src/main/java/com/example/grow/Login_page.java
@@ -4,10 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login_page extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +31,29 @@ public class Login_page extends AppCompatActivity {
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        TextView register = (TextView) findViewById(R.id.register);
-        register.setOnClickListener(view -> {
-            startActivity(new Intent(Login_page.this, Register_page.class));
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = firebaseAuth -> {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user != null) {
+                startActivity(new Intent(Login_page.this, Main.class));
+            }
+        };
+
+        TextView register = findViewById(R.id.register);
+        register.setOnClickListener(x -> startActivity(new Intent(Login_page.this, Register_page.class)));
+
+        Button login = findViewById(R.id.btn_login);
+        login.setOnClickListener(x -> {
+            String email = ((EditText)findViewById(R.id.editTextTextEmailAddress)).getText().toString();
+            String password = ((EditText)findViewById(R.id.editTextTextPassword)).getText().toString();
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, task -> {
+                if(task.isSuccessful()) {
+                    Toast.makeText(Login_page.this, "Authorized successfully", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(Login_page.this, Main.class));
+                } else {
+                    Toast.makeText(Login_page.this, "Wrong e-mail or password", Toast.LENGTH_LONG).show();
+                }
+            });
         });
     }
 }
--- app/src/main/java/com/example/grow/Register_page.java
+++ app/src/main/java/com/example/grow/Register_page.java
@@ -2,14 +2,25 @@ package com.example.grow;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Register_page extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +29,24 @@ public class Register_page extends AppCompatActivity {
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        TextView login = (TextView)findViewById(R.id.log);
+        mAuth = FirebaseAuth.getInstance();
 
-        TextView register = (TextView) findViewById(R.id.log);
-        register.setOnClickListener(view -> {
-            finish();
-            //startActivity(new Intent(Register_page.this, Login_page.class));
+        TextView login = (TextView)findViewById(R.id.log);
+        login.setOnClickListener(x -> finish());
+
+        Button register = findViewById(R.id.btn_register);
+        register.setOnClickListener(x -> {
+            String email = ((EditText)findViewById(R.id.editTextTextEmailAddress2)).getText().toString();
+            String password = ((EditText)findViewById(R.id.editTextTextPassword2)).getText().toString();
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+                if(task.isSuccessful()) {
+                    Toast.makeText(Register_page.this, "Registered successfully", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                else {
+                    Toast.makeText(Register_page.this, "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+            });
         });
     }
 }
