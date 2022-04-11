2b73e6bfc0366550fc11c421e942285f91553a40
denios.vasilios@gmail.com <denios.vasilios@gmail.com>
2022-04-11 11:05:01 +0300

    Naming fixes.

--- app/src/main/java/com/example/grow/CalendarActivity.java
+++ app/src/main/java/com/example/grow/CalendarActivity.java
@@ -6,12 +6,12 @@ import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Progress_calendar_page extends AppCompatActivity {
+public class CalendarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.progress_calendar_page);
+        setContentView(R.layout.activity_calendar);
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
